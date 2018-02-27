8b0737e4aa92de798395a377d95da0d6e625c8f2
Hiroshi Okada <okadahiroshi@miobox.jp>
2018-02-27 14:05:28 +0900

    最初のコミット

---
+++ README.md
@@ -0,0 +1,16 @@
+# hiroshi-toycode-com
+
+hiroshi.toycode.com のソースリポジトリ
+
+## 必要なもの
+
+nodejs >= 8.X
+
+s3 + cloudfront
+
+## インストールとローカルでの実行
+
+npm install
+npm run build
+npm run watch
+
---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+const gulp = require('gulp')
+const pug = require('gulp-pug')
+const styleInject = require('gulp-style-inject')
+const cleanCSS = require('gulp-clean-css')
+const htmlImg64 = require('gulp-html-img64')
+
+gulp.task('index', ['cleanCSS'], () =>
+  gulp
+    .src('./index.pug')
+    .pipe(pug())
+    .pipe(styleInject())
+    .pipe(htmlImg64())
+    .pipe(gulp.dest('dist'))
+)
+
+gulp.task('cleanCSS', () =>
+  gulp
+    .src('./css/*.css')
+    .pipe(cleanCSS())
+    .pipe(gulp.dest('tmp'))
+)
+
+gulp.task('icons', () =>
+  gulp.src('./icon/*.*').pipe(gulp.dest('./dist/icon'))
+)
+
+gulp.task('default', ['index', 'cleanCSS', 'icons'])
+
+gulp.task('watch', () => {
+  gulp.watch('./index.pug', ['index'])
+  gulp.watch('./css/*.css', ['index', 'cleanCSS'])
+})
